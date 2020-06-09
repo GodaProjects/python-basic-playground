dc57238666d3015eec0ab6780fa59d0159573a12
GodaProjects <goda.projects@gmail.com>
2020-06-09 09:02:08 +0530

    Dictionary

--- goda2.py
+++ goda2.py
@@ -186,3 +186,30 @@ print(next(iterator))
 print(next(iterator))
 
 # Tuple is an immutable list - not trying now. Anything we can do with list can be done with tuple except modifying them.
+
+# Dictionary or key value pairs
+friends = {
+    "Goda": "Chellam",
+    "Tanu": "Kutty"
+}
+print(friends)
+friends1 = dict([
+    ("Goda", "Chellam"),
+    ("Tanu", "Kutty")
+])
+print(friends1)
+print(friends1["Goda"])
+print(friends1.items())
+
+del friends1["Tanu"]
+print(friends1.items())
+friends1.pop("Goda")
+print(friends1.items())
+
+# Key and value printing
+for k in friends:
+    print(k)
+for v in friends.values():
+    print(v)
+
+print("%(Goda)s is %(Tanu)s's friend" % friends)

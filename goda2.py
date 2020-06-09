30c2e574abe1dcfa8c63786a623d97573e2a0348
GodaProjects <goda.projects@gmail.com>
2020-06-09 09:08:05 +0530

    Set

--- goda2.py
+++ goda2.py
@@ -213,3 +213,26 @@ for v in friends.values():
     print(v)
 
 print("%(Goda)s is %(Tanu)s's friend" % friends)
+
+# Set
+set1 = set(["Goda", 1])
+set2 = {"Tanu", 2, 2}
+
+print(set1)
+print(set2)
+
+set3 = set1 | set2
+print(set3)
+set3.add("Rama")
+print(set3)
+print(set3.pop())
+set3 |= set2
+print(set3)
+set3 = set3.intersection(set2)
+print(set3)
+set3 = set1 | set2
+set3 = set3.symmetric_difference(set2)
+print(set3)
+
+set3 = set3.difference(set2)
+print(set3)
